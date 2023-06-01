9e96ad129baacf9cc5b4fdb87994e966d1807d9e
CommunityALL <viktoriacobko@gmail.com>
2023-06-01 11:05:38 +0300

    commit

---
+++ src/main/java/com/site/blog/repo/PostRepository.java
@@ -0,0 +1,8 @@
+package com.site.blog.repo;
+
+import com.site.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
